b3348bc4a6d652596ca4eceb88bb593222c77fef
Kiran Shila <me@kiranshila.com>
2024-03-01 11:48:27 -0800

    add z0 and gamma

--- src/math/utils.jl
+++ src/math/utils.jl
@@ -3,3 +3,9 @@ dB(x::Complex) = 10 * log10(abs(x))
 
 """The voltage reflection coefficient"""
 Γ(Z::Number, Z0::Number) = (Z - Z0) / (Z + Z0)
+
+"""The characteristic impedance of a transmisslion line given the RLGC parameters and frequency"""
+Z₀(r::Real, l::Real, g::Real, c::Real, f::Real) = sqrt((r + 2π * im * f * l) / (g + 2π * im * f * c))
+
+"""The propogation constant of a transmisslion line given the RLGC parameters and frequency"""
+γ(r::Real, l::Real, g::Real, c::Real, f::Real) = sqrt((r + 2π * im * f * l) * (g + 2π * im * f * c))
